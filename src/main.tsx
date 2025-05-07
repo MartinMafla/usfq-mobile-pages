068c8498931bba7717f7be65d4fbc305a8db8f4a
Martin Mafla <martin_ale_mafla@hotmail.com>
2025-05-07 10:31:36 -0500

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+// src/main.tsx (modificado)
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/usfq-mobile-pages">
+    <App />
+  </BrowserRouter>
+);
